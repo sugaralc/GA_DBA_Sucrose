e48bff41b4545e91016b975d9880ca5d7afd8057
sugaralc <gus_lara63@hotmail.com>
2024-04-30 22:12:59 -0500

    Score function with scalarized ranges

--- Score_function.py
+++ Score_function.py
@@ -1,4 +1,5 @@
 from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 scaler = MinMaxScaler()
 c1 = 0.5
 c2 = 0.2
@@ -10,14 +11,19 @@ def score_function(energies,LogPs,pKas_max,sa_scores):
     for g,logp,pka,sa in zip(energies,LogPs,pKas_max,sa_scores):
         data.append([g,logp,pka,sa])
 
-    model=scaler.fit(data)
-    scaled_data=model.transform(data)
+    #Adding the extreme values to have the same scale in all the iterations
+    data.append([-15,-3,3,1])
+    data.append([3,6,10,10])
+
+    model = scaler.fit(data)
+    scaled_data = model.transform(data)
+    #print(scaled_data)
     scores = []
-    for ind_scaled in scaled_data:
-        G_scaled = ind_scaled[0]
-        LogP_scaled = ind_scaled[1]
-        pKa_scaled = ind_scaled[2]
-        SA_scaled = ind_scaled[3]
+    for i in range(len(scaled_data)-2):
+        G_scaled = scaled_data[i][0]
+        LogP_scaled = scaled_data[i][1]
+        pKa_scaled = scaled_data[i][2]
+        SA_scaled = scaled_data[i][3]
 
         ind_score = (c1*G_scaled + c2*LogP_scaled + c3*pKa_scaled + c4*SA_scaled)
         scores.append(ind_score)
