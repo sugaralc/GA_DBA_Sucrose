1454cedc8b0d4462a4b70fbbf5a2e9b23e7917d3
sugaralc <gus_lara63@hotmail.com>
2024-05-17 11:33:17 -0500

    Five rings PBAs deleted

--- PBA_mutation.py
+++ PBA_mutation.py
@@ -2,11 +2,11 @@
 Written by Gustavo Lara-Cruz 2024
 '''
 
-from rdkit import Chem, DataStructs
-from rdkit.Chem import AllChem
-from random import randrange
+from rdkit import Chem#, DataStructs
+#from rdkit.Chem import AllChem
+#from random import randrange
 
-import random
+#import random
 import numpy as np
 
 from rdkit import rdBase
@@ -23,7 +23,7 @@ def Glcyl():
 
     if ph_subs == 'meta':
 
-        PBA_meta=[['C-O-C1=CC=C(-[96*])=CC1-[98*]', 9.0,-1], # 2-Methoxyphenylboronic acid DOI:10.1016/j.tet.2004.08.051
+        PBA_meta=[['C-O-C1=CC=C(-[96*])C=C1-[98*]', 9.0,-1], # 2-Methoxyphenylboronic acid DOI:10.1016/j.tet.2004.08.051
         ['N-C1=CC(-[96*])=CC(-[98*])=C1', 8.9,-1], # 3-Aminophenylboronic acid DOI:10.1016/j.tet.2004.08.051
         ['C1=CC(-[96*])=CC(-[98*])=C1', 8.8,-1], # Phenylboronic acid DOI:10.1016/j.tet.2004.08.051
         ['FC1=CC=C(-[98*])C=C1-[96*]', 8.6,-1], # 4-Fluorophenylboronic acid DOI:10.1016/j.tet.2004.08.051 
@@ -45,7 +45,7 @@ def Glcyl():
         ['FC1=CC(-[98*])=C(F)C(-[96*])=C1F', 6.8,-1], # 3,4,5-Trifluorophenylboronic acid DOI:10.1016/j.tet.2004.08.051
         ['FC1=C(F)C(-[96*])=CC(-[98*])=C1F', 6.8,-1], # 2,3,4-Trifluorophenylboronic acid DOI:10.1016/j.tet.2004.08.051
         ['FC1=CC(-[98*])=C(F)C(-[96*])=C1F', 6.7,-1], # 2,4,5-Trifluorophenylboronic acid DOI:10.1016/j.tet.2004.08.051
-        ['C-[N+](-C)-C-C1=CC=C(-[96*])C=C1-[98*]', 5.3,-1], # 2-Dimethylaminomethylphenylboronic acid (DAPBA) DOI:10.1016/j.tet.2004.08.051
+        ['C-[NH+](-C)-C-C1=CC=C(-[96*])C=C1-[98*]', 5.3,-1], # 2-Dimethylaminomethylphenylboronic acid (DAPBA) DOI:10.1016/j.tet.2004.08.051
         ['[O-]-[N+](=O)-C1=CC(-[96*])=C(F)C(-[98*])=C1', 6.0,-1], # 2-Fluoro-5-nitrophenylboronic acid DOI:10.1016/j.tet.2004.08.051
         ['C-C1=CC(-[98*])=C[N+](-[#6]-[96*])=C1', 4.4,-1], # 5-Methylpyridine-3-boronic acid DOI:10.1016/j.tet.2004.08.051
         ['[96*]-C-[N+]1=CC=CC(-[98*])=C1', 4.4,-1], # 3-Methylpyridineboronic acid  DOI:10.1016/j.tet.2004.08.051
@@ -59,7 +59,7 @@ def Glcyl():
         ['[96*]-C1=CC(-[98*])=CC=C1-C(=O)-N-C-C-N-C(=O)-C=C', 7.8,-1], # 4-(1,6-dioxo-2,5-diaza-7-oxamyl-)-phenylboronic acid (DDOPBA) DOI:10.1039/B920319B Bonding to silicon trough double bond in alkyl chain See the review DOI: 10.1039/C5CS00013K
         ['FC1=C(-[96*])C=C(-[98*])C(F)=C1-C=O', 6.5,-1], # 2,4-difluoro-3-formyl-phenylboronicacid (DFFPBA) See the review DOI: 10.1039/C5CS00013K
         #['[96*]-c1ccc2-[#6]-[#8]-[98*]-c2c1', 7.2,0], # Benzoboroxole DOI: 10.1021/jo800788s Bonding with trans 4-6 diols prefered over trans 3-4 diol as in glucose.
-        ['[96*]-C-1=C-C(-[98*])=C-S-1', 8.1 ,-1], # 3-thiopeneboronic acid DOI: 10.3390/chemosensors10070251 Read this paper, has some thermochemical measures
+        #['[96*]-C-1=C-C(-[98*])=C-S-1', 8.1 ,-1], # 3-thiopeneboronic acid DOI: 10.3390/chemosensors10070251 Read this paper, has some thermochemical measures
         ['FC1=NC=C(-[96*])C=C1-[98*]', 7.1,-1], # 2-fluoro-3-pyridylboronic acid or 2F-3-PyBA DOI: 10.1021/ol5036003
         ['FC1=NC=C(-[98*])C=C1-[96*]', 7.0,-1], # 2-fluoro-5-pyridylboronic acid or 2F-5-PyBA  DOI: 10.1021/ol5036003
         ['[96*]-C-[N+]1=CN=CC(-[98*])=C1', 6.2,-1], # pyrimidine-5-boronic acid DOI:10.1039/c7sc01905j
@@ -78,7 +78,7 @@ def Glcyl():
         index = np.random.choice(list(range(len(PBA_meta))), p = p)
         PBA_mol, pKa, dummy = PBA_meta[index]
 
-
+        #print("meta",index,Chem.MolFromSmiles(PBA_mol))
         return Chem.MolFromSmiles(PBA_mol), str(pKa)
 
     if ph_subs == 'ortho':
@@ -129,8 +129,8 @@ def Glcyl():
         ['FC1=CC(-[96*])=C(-[98*])C(F)=C1-C=O', 6.5,-1], # 2,4-difluoro-3-formyl-phenylboronicacid (DFFPBA) See the review DOI: 10.1039/C5CS00013K
         #['[96*]-c1cccc2-[#6]-[#8]-[98*]-c12', 7.2,0], # Benzoboroxole DOI: 10.1021/jo800788s Bonding with trans 4-6 diols prefered over trans 3-4 diol as in glucose.
         #['[#8]-[#6](=O)-c1ccc2-[#6]-[#8]-[98*]-c2c1-[96*]', 7.2,0], # Benzoboroxolebenzaldehide Binding to polimer throug carboxilic acid. Asummed to be similar to Benzoboroxole DOI: 10.1021/jo800788s
-        ['[96*]-C1=C-S-C=C1-[98*]', 8.1,-1], # 3-thiopeneboronic acid DOI: 10.3390/chemosensors10070251 Read this paper, has some thermochemical measures
-        ['[96*]-C1=C(-[98*])-C=C-S1', 8.1,-1], # 3-thiopeneboronic acid DOI: 10.3390/chemosensors10070251 Read this paper, has some thermochemical measures
+        #['[96*]-C1=C-S-C=C1-[98*]', 8.1,-1], # 3-thiopeneboronic acid DOI: 10.3390/chemosensors10070251 Read this paper, has some thermochemical measures
+        #['[96*]-C1=C(-[98*])-C=C-S1', 8.1,-1], # 3-thiopeneboronic acid DOI: 10.3390/chemosensors10070251 Read this paper, has some thermochemical measures
         ['FC1=NC=CC(-[96*])=C1-[98*]', 7.1,-1], # 2-fluoro-3-pyridylboronic acid or 2F-3-PyBA   DOI: 10.1021/ol5036003
         ['FC1=CC(-[96*])=C(-[98*])C=N1', 7.0,-1], # 2-fluoro-5-pyridylboronic acid or 2F-5-PyBA  DOI: 10.1021/ol5036003
         ['FC1=CC=C(-[98*])C(-[96*])=N1', 7.0,-1], # 2-fluoro-5-pyridylboronic acid or 2F-5-PyBA DOI: 10.1021/ol5036003
@@ -152,6 +152,7 @@ def Glcyl():
         index = np.random.choice(list(range(len(PBA_ortho))), p = p)
         PBA_mol, pKa, dummy = PBA_ortho[index]
 
+        #print("ortho",Chem.MolFromSmiles(PBA_mol))
         return Chem.MolFromSmiles(PBA_mol), str(pKa)
 
     if ph_subs == 'para':
@@ -183,6 +184,7 @@ def Glcyl():
         index = np.random.choice(list(range(len(PBA_para))), p = p)
         PBA_mol, pKa, dummy = PBA_para[index]
 
+        #print("para",Chem.MolFromSmiles(PBA_mol))
         return Chem.MolFromSmiles(PBA_mol), str(pKa)
 
 def Frcyl():
@@ -193,7 +195,7 @@ def Frcyl():
 
     if ph_subs == 'meta':
 
-        PBA_meta=[['C-O-C1=CC=C(-[97*])=CC1-[99*]', 9.0,-1], # 2-Methoxyphenylboronic acid DOI:10.1016/j.tet.2004.08.051
+        PBA_meta=[['C-O-C1=CC=C(-[97*])C=C1-[99*]', 9.0,-1], # 2-Methoxyphenylboronic acid DOI:10.1016/j.tet.2004.08.051
         ['N-C1=CC(-[97*])=CC(-[99*])=C1', 8.9,-1], # 3-Aminophenylboronic acid DOI:10.1016/j.tet.2004.08.051
         ['C1=CC(-[97*])=CC(-[99*])=C1', 8.8,-1], # Phenylboronic acid DOI:10.1016/j.tet.2004.08.051
         ['FC1=CC=C(-[99*])C=C1-[97*]', 8.6,-1], # 4-Fluorophenylboronic acid DOI:10.1016/j.tet.2004.08.051 
@@ -215,7 +217,8 @@ def Frcyl():
         ['FC1=CC(-[99*])=C(F)C(-[97*])=C1F', 6.8,-1], # 3,4,5-Trifluorophenylboronic acid DOI:10.1016/j.tet.2004.08.051
         ['FC1=C(F)C(-[97*])=CC(-[99*])=C1F', 6.8,-1], # 2,3,4-Trifluorophenylboronic acid DOI:10.1016/j.tet.2004.08.051
         ['FC1=CC(-[99*])=C(F)C(-[97*])=C1F', 6.7,-1], # 2,4,5-Trifluorophenylboronic acid DOI:10.1016/j.tet.2004.08.051
-        ['C-[N+](-C)-C-C1=CC=C(-[97*])C=C1-[99*]', 5.3,-1], # 2-Dimethylaminomethylphenylboronic acid (DAPBA) DOI:10.1016/j.tet.2004.08.051
+        #['C-[NH+](-C)-C-C1=CC=C(-[97*])C=C1-[99*]', 5.3,-1], # 2-Dimethylaminomethylphenylboronic acid (DAPBA) DOI:10.1016/j.tet.2004.08.051
+        ['[99*]-C1C(-C[NH+](C)C)=CC(-[97*])=CC1', 5.3,-1], # 2-Dimethylaminomethylphenylboronic acid (DAPBA) DOI:10.1016/j.tet.2004.08.051
         ['[O-]-[N+](=O)-C1=CC(-[97*])=C(F)C(-[99*])=C1', 6.0,-1], # 2-Fluoro-5-nitrophenylboronic acid DOI:10.1016/j.tet.2004.08.051
         ['C-C1=CC(-[99*])=C[N+](-[#6]-[97*])=C1', 4.4,-1], # 5-Methylpyridine-3-boronic acid DOI:10.1016/j.tet.2004.08.051
         ['[97*]-C-[N+]1=CC=CC(-[99*])=C1', 4.4,-1], # 3-Methylpyridineboronic acid  DOI:10.1016/j.tet.2004.08.051
@@ -229,7 +232,7 @@ def Frcyl():
         ['[97*]-C1=CC(-[99*])=CC=C1-C(=O)-N-C-C-N-C(=O)-C=C', 7.8,-1], # 4-(1,6-dioxo-2,5-diaza-7-oxamyl-)-phenylboronic acid (DDOPBA) DOI:10.1039/B920319B Bonding to silicon trough double bond in alkyl chain See the review DOI: 10.1039/C5CS00013K
         ['FC1=C(-[97*])C=C(-[99*])C(F)=C1-C=O', 6.5,-1], # 2,4-difluoro-3-formyl-phenylboronicacid (DFFPBA) See the review DOI: 10.1039/C5CS00013K
         #['[97*]-c1ccc2-[#6]-[#8]-[99*]-c2c1', 7.2,0], # Benzoboroxole DOI: 10.1021/jo800788s Bonding with trans 4-6 diols prefered over trans 3-4 diol as in glucose.
-        ['[97*]-C-1=C-C(-[99*])=C-S-1', 8.1 ,-1], # 3-thiopeneboronic acid DOI: 10.3390/chemosensors10070251 Read this paper, has some thermochemical measures
+        #['[97*]-C-1=C-C(-[99*])=C-S-1', 8.1 ,-1], # 3-thiopeneboronic acid DOI: 10.3390/chemosensors10070251 Read this paper, has some thermochemical measures
         ['FC1=NC=C(-[97*])C=C1-[99*]', 7.1,-1], # 2-fluoro-3-pyridylboronic acid or 2F-3-PyBA DOI: 10.1021/ol5036003
         ['FC1=NC=C(-[99*])C=C1-[97*]', 7.0,-1], # 2-fluoro-5-pyridylboronic acid or 2F-5-PyBA  DOI: 10.1021/ol5036003
         ['[97*]-C-[N+]1=CN=CC(-[99*])=C1', 6.2,-1], # pyrimidine-5-boronic acid DOI:10.1039/c7sc01905j
@@ -299,8 +302,8 @@ def Frcyl():
         ['FC1=CC(-[97*])=C(-[99*])C(F)=C1-C=O', 6.5,-1], # 2,4-difluoro-3-formyl-phenylboronicacid (DFFPBA) See the review DOI: 10.1039/C5CS00013K
         #['[97*]-c1cccc2-[#6]-[#8]-[99*]-c12', 7.2,0], # Benzoboroxole DOI: 10.1021/jo800788s Bonding with trans 4-6 diols prefered over trans 3-4 diol as in glucose.
         #['[#8]-[#6](=O)-c1ccc2-[#6]-[#8]-[99*]-c2c1-[97*]', 7.2,0], # Benzoboroxolebenzaldehide Binding to polimer throug carboxilic acid. Asummed to be similar to Benzoboroxole DOI: 10.1021/jo800788s
-        ['[97*]-C1=C-S-C=C1-[99*]', 8.1,-1], # 3-thiopeneboronic acid DOI: 10.3390/chemosensors10070251 Read this paper, has some thermochemical measures
-        ['[97*]-C1=C(-[99*])-C=C-S1', 8.1,-1], # 3-thiopeneboronic acid DOI: 10.3390/chemosensors10070251 Read this paper, has some thermochemical measures
+        #['[97*]-C1=C-S-C=C1-[99*]', 8.1,-1], # 3-thiopeneboronic acid DOI: 10.3390/chemosensors10070251 Read this paper, has some thermochemical measures
+        #['[97*]-C1=C(-[99*])-C=C-S1', 8.1,-1], # 3-thiopeneboronic acid DOI: 10.3390/chemosensors10070251 Read this paper, has some thermochemical measures
         ['FC1=NC=CC(-[97*])=C1-[99*]', 7.1,-1], # 2-fluoro-3-pyridylboronic acid or 2F-3-PyBA   DOI: 10.1021/ol5036003
         ['FC1=CC(-[97*])=C(-[99*])C=N1', 7.0,-1], # 2-fluoro-5-pyridylboronic acid or 2F-5-PyBA  DOI: 10.1021/ol5036003
         ['FC1=CC=C(-[99*])C(-[97*])=N1', 7.0,-1], # 2-fluoro-5-pyridylboronic acid or 2F-5-PyBA DOI: 10.1021/ol5036003
